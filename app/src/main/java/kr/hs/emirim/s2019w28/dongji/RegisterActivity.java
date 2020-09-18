9c37195272bd3039067c3fe1f733556c586aa1df
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-09-18 18:24:46 +0900

    회원가입 기능 완료

--- app/src/main/java/kr/hs/emirim/s2019w28/dongji/RegisterActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019w28/dongji/RegisterActivity.java
@@ -1,14 +1,74 @@
 package kr.hs.emirim.s2019w28.dongji;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText reg_email;
+    private EditText reg_password;
+    private EditText reg_confirm_password;
+    private Button reg_btn;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        reg_email = (EditText) findViewById(R.id.reg_email);
+        reg_password = (EditText) findViewById(R.id.reg_password);
+        reg_confirm_password = (EditText) findViewById(R.id.reg_confirm_password);
+        reg_btn = (Button) findViewById(R.id.reg_btn);
+
+        reg_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = reg_email.getText().toString();
+                String pass = reg_password.getText().toString();
+                String confirm_pass = reg_confirm_password.getText().toString();
+
+                if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(pass) && !TextUtils.isEmpty(confirm_pass)) {
+
+                    if (pass.equals(confirm_pass)) {
+
+                        firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+
+                                    Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                                    startActivity(mainIntent);
+                                    finish();
+
+                                } else {
+                                    String errorMessage = task.getException().getMessage();
+                                    Toast.makeText(RegisterActivity.this, "Error: " + errorMessage, Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "다시 입력한 비밀번호와 기존 비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
     }
 }
