cdaa45bd08ce753de2ab8427b7c6263125f60764
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-09-18 17:27:33 +0900

    Firebase Auth 연동 완료

--- app/src/main/java/kr/hs/emirim/s2019w28/dongji/LoginActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019w28/dongji/LoginActivity.java
@@ -3,8 +3,16 @@ package kr.hs.emirim.s2019w28.dongji;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText login_email;
+    private EditText login_password;
+    private Button login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
