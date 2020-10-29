3ad0ac640b29a1fa3336b6d073b0de2db295b3f7
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-10-30 02:04:05 +0900

    detail page design 변경 (#19)

--- app/src/main/java/kr/hs/emirim/s2019w28/dongji/DetailPageActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019w28/dongji/DetailPageActivity.java
@@ -3,19 +3,35 @@ package kr.hs.emirim.s2019w28.dongji;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.util.List;
 
 import kr.hs.emirim.s2019w28.dongji.model.Comments;
 
-public class DetailPageActivity extends AppCompatActivity {
+public class DetailPageActivity extends AppCompatActivity implements View.OnClickListener {
 
     private RecyclerView comment_list_view;
+    private ImageView detail_back_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_page);
+
+        detail_back_btn = findViewById(R.id.detail_back_btn);
+
+        detail_back_btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.detail_back_btn :
+                finish();
+        }
     }
 }
