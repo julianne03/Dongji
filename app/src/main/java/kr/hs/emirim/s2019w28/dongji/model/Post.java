5c6dd29531eb7b0f0b8196ef4006e41be576a1d5
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-10-30 01:09:01 +0900

    Post 리스트 내용 디자인 변경 (#15)

--- app/src/main/java/kr/hs/emirim/s2019w28/dongji/model/Post.java
+++ app/src/main/java/kr/hs/emirim/s2019w28/dongji/model/Post.java
@@ -3,15 +3,16 @@ package kr.hs.emirim.s2019w28.dongji.model;
 import java.util.Date;
 
 public class Post extends kr.hs.emirim.s2019w28.dongji.model.PostId {
-    public String post_title,post_content,post_image;
+    public String post_title,post_content,virus_category,post_image;
     public Date timestamp;
 
     public Post() {
     }
 
-    public Post(String post_title, String post_content, String post_image, Date timestamp) {
+    public Post(String post_title, String post_content, String virus_category, String post_image, Date timestamp) {
         this.post_title = post_title;
         this.post_content = post_content;
+        this.virus_category = virus_category;
         this.post_image = post_image;
         this.timestamp = timestamp;
     }
@@ -34,6 +35,14 @@ public class Post extends kr.hs.emirim.s2019w28.dongji.model.PostId {
 
     public void setPost_content(String post_content) {
         this.post_content = post_content;
+    }
+
+    public String getVirus_category() {
+        return virus_category;
+    }
+
+    public void setVirus_category(String virus_category) {
+        this.virus_category = virus_category;
     }
 
     public String getPost_image() {
