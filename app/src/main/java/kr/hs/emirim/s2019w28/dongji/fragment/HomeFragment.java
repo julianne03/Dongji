e726a7193b22b2b82d1489c41b3e51b781729989
seobin Park <seobin0203@naver.com>
2020-10-29 22:26:06 +0900

    #15 스토리 추가 기능 구현

--- app/src/main/java/kr/hs/emirim/s2019w28/dongji/fragment/HomeFragment.java
+++ app/src/main/java/kr/hs/emirim/s2019w28/dongji/fragment/HomeFragment.java
@@ -8,15 +8,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import kr.hs.emirim.s2019w28.dongji.AddStoryActivity;
 import kr.hs.emirim.s2019w28.dongji.NewPostActivity;
 import kr.hs.emirim.s2019w28.dongji.R;
 
 public class HomeFragment extends Fragment {
 
     private FloatingActionButton add_post;
+    private ImageButton add_story;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -42,6 +45,17 @@ public class HomeFragment extends Fragment {
             public void onClick(View v) {
                 Intent newPostIntent = new Intent(getContext(), NewPostActivity.class);
                 startActivity(newPostIntent);
+            }
+        });
+
+
+        add_story = mView.findViewById(R.id.add_story);
+
+        add_story.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newStoryIntent = new Intent(getContext(), AddStoryActivity.class);
+                startActivity(newStoryIntent);
             }
         });
 
