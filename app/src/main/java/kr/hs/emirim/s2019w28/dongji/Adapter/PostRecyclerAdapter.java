58e66ee791aa91d1b112fe8c55dfcb0855ea3a14
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-11-01 23:13:58 +0900

    help icon add (#15) (#19)

--- app/src/main/java/kr/hs/emirim/s2019w28/dongji/Adapter/PostRecyclerAdapter.java
+++ app/src/main/java/kr/hs/emirim/s2019w28/dongji/Adapter/PostRecyclerAdapter.java
@@ -12,16 +12,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import kr.hs.emirim.s2019w28.dongji.DetailPageActivity;
 import kr.hs.emirim.s2019w28.dongji.R;
