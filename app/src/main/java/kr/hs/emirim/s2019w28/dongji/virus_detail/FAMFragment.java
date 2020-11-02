6a559373a2ccb68c3d14c4fd8554c60b1cf1f5ea
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-11-02 10:26:45 +0900

    카테고리 동작 구현 완료 (#8)

--- app/src/main/java/kr/hs/emirim/s2019w28/dongji/virus_detail/FAMFragment.java
+++ app/src/main/java/kr/hs/emirim/s2019w28/dongji/virus_detail/FAMFragment.java
@@ -2,65 +2,114 @@ package kr.hs.emirim.s2019w28.dongji.virus_detail;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.hs.emirim.s2019w28.dongji.Adapter.PostRecyclerAdapter;
 import kr.hs.emirim.s2019w28.dongji.R;
+import kr.hs.emirim.s2019w28.dongji.model.Post;
+import kr.hs.emirim.s2019w28.dongji.model.User;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FAMFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FAMFragment extends Fragment {
+    private RecyclerView post_list_view;
+    private List<Post> post_list;
+    private List<User> user_list;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FirebaseFirestore firebaseFirestore;
+    private PostRecyclerAdapter postRecyclerAdapter;
+    private FirebaseAuth firebaseAuth;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public FAMFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FAMFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FAMFragment newInstance(String param1, String param2) {
-        FAMFragment fragment = new FAMFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fam, container, false);
+        View mView = inflater.inflate(R.layout.fragment_fam, container, false);
+
+        post_list = new ArrayList<>();
+        user_list = new ArrayList<>();
+        post_list_view = mView.findViewById(R.id.post_list_view_fam);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        postRecyclerAdapter = new PostRecyclerAdapter(post_list,user_list);
+        post_list_view.setLayoutManager(new LinearLayoutManager(getActivity()));
+        post_list_view.setAdapter(postRecyclerAdapter);
+
+        if(firebaseAuth.getCurrentUser() != null) {
+
+            firebaseFirestore = FirebaseFirestore.getInstance();
+
+            Query query = firebaseFirestore.collection("Posts")
+                    .whereEqualTo("virus_category","기타");
+
+
+            query.addSnapshotListener(new EventListener<QuerySnapshot>() {
+                @Override
+                public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                    if(firebaseAuth.getCurrentUser() != null) {
+                        if(!value.isEmpty()) {
+                            post_list.clear();
+                            user_list.clear();
+
+                        }
+                        if(error != null) {
+                            System.err.println(error);
+                        }
+
+                        for(DocumentChange doc : value.getDocumentChanges()) {
+
+                            if(doc.getType() == DocumentChange.Type.ADDED) {
+                                String post_id = doc.getDocument().getId();
+                                final Post post = doc.getDocument().toObject(Post.class).withId(post_id);
+
+                                String postUserId = doc.getDocument().getString("user_id");
+                                firebaseFirestore.collection("Users").document(postUserId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                        if(task.isSuccessful()) {
+                                            User user = task.getResult().toObject(User.class);
+
+                                            post_list.add(post);
+                                            user_list.add(user);
+                                        }
+                                        postRecyclerAdapter.notifyDataSetChanged();
+                                    }
+                                });
+                            }
+                        }
+
+                    }
+                }
+            });
+        }
+
+        return mView;
     }
 }
