051e5485ddc6905abf1775e3c61b1b0ad99925e0
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-11-05 00:26:23 +0900

    StoryRecyclerAdapter 구현 완료 (#15)

--- app/src/main/java/kr/hs/emirim/s2019w28/dongji/Adapter/StoryRecyclerAdapter.java
+++ app/src/main/java/kr/hs/emirim/s2019w28/dongji/Adapter/StoryRecyclerAdapter.java
@@ -1,33 +1,39 @@
 package kr.hs.emirim.s2019w28.dongji.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.mikhaellopez.circularimageview.CircularImageView;
 
 import java.util.List;
 
 import kr.hs.emirim.s2019w28.dongji.R;
-import kr.hs.emirim.s2019w28.dongji.model.Story;
+import kr.hs.emirim.s2019w28.dongji.StoryDetailActivity;
 import kr.hs.emirim.s2019w28.dongji.model.User;
 
 public class StoryRecyclerAdapter extends RecyclerView.Adapter<StoryRecyclerAdapter.ViewHolder> {
 
-    private List<Story> story_list;
     private List<User> user_list;
     private Context context;
 
     private FirebaseFirestore firebaseFirestore;
     private FirebaseAuth firebaseAuth;
 
-    public StoryRecyclerAdapter(List<Story> story_list, List<User> user_list) {
-        this.story_list = story_list;
+    public StoryRecyclerAdapter(List<User> user_list) {
         this.user_list = user_list;
     }
 
@@ -46,6 +52,25 @@ public class StoryRecyclerAdapter extends RecyclerView.Adapter<StoryRecyclerAdap
     public void onBindViewHolder(@NonNull StoryRecyclerAdapter.ViewHolder holder, int position) {
         holder.setIsRecyclable(false);
 
+        holder.findId();
+
+        final String user_name = user_list.get(position).getName();
+        final String user_image = user_list.get(position).getImage();
+
+        holder.setUser(user_name,user_image);
+
+        holder.story_item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent story_detail = new Intent(context, StoryDetailActivity.class);
+                story_detail.putExtra("user_name",user_name);
+                story_detail.putExtra("user_image",user_image);
+                context.startActivity(story_detail);
+            }
+        });
+
+
+
     }
 
     @Override
@@ -57,10 +82,33 @@ public class StoryRecyclerAdapter extends RecyclerView.Adapter<StoryRecyclerAdap
 
         private View mView;
 
+        private ConstraintLayout story_item;
+        private CircularImageView user_image;
+        private TextView user_nickname;
+
 
         public ViewHolder(View v) {
             super(v);
             mView = v;
+        }
+
+        public void findId() {
+            story_item = mView.findViewById(R.id.story_item);
+            user_image = mView.findViewById(R.id.story_user_image);
+            user_nickname = mView.findViewById(R.id.story_user_nickname);
+
+        }
+
+        public void setUser(String name,String image) {
+            user_nickname.setText(name);
+            RequestOptions requestOptions = new RequestOptions();
+            requestOptions.placeholder(R.drawable.ic_baseline_account_circle_24);
+
+            Glide.with(context)
+                    .applyDefaultRequestOptions(requestOptions)
+                    .load(Uri.parse(image))
+                    .thumbnail()
+                    .into(user_image);
         }
 
 
