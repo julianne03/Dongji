46c5f9f331931cbe943ecbaccaf373515a75b118
seobin Park <seobin0203@naver.com>
2020-11-09 22:06:29 +0900

    #8 필요 없는 코드 정리

--- app/src/main/java/kr/hs/emirim/s2019w28/dongji/virus_detail/more_info/Ai/aFragment4.java
+++ app/src/main/java/kr/hs/emirim/s2019w28/dongji/virus_detail/more_info/Ai/aFragment4.java
@@ -10,51 +10,17 @@ import android.view.ViewGroup;
 
 import kr.hs.emirim.s2019w28.dongji.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link aFragment4#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class aFragment4 extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public aFragment4() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment aFragment4.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static aFragment4 newInstance(String param1, String param2) {
-        aFragment4 fragment = new aFragment4();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
--- app/src/main/java/kr/hs/emirim/s2019w28/dongji/virus_detail/more_info/Etc/eFragment3.java
+++ app/src/main/java/kr/hs/emirim/s2019w28/dongji/virus_detail/more_info/Etc/eFragment3.java
@@ -10,51 +10,18 @@ import android.view.ViewGroup;
 
 import kr.hs.emirim.s2019w28.dongji.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link eFragment3#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class eFragment3 extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public eFragment3() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment eFragment3.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static eFragment3 newInstance(String param1, String param2) {
-        eFragment3 fragment = new eFragment3();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
